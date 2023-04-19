ca8d0003514ead99b4e0bb2b6a885468b4a8d85a
yoyoshingu <leo@shingu.ac.kr>
2023-04-19 10:08:52 +0900

    1st project setup

---
+++ src/main/java/com/sg/leoublog/Ublog2023Application.java
@@ -0,0 +1,13 @@
+package com.sg.leoublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ublog2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ublog2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/leoublog/Ublog2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.leoublog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ublog2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
